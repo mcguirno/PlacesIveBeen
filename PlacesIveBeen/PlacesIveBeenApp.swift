e1b12a14edca061ed95385b88afae74fb72ac54f
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-04-07 16:33:47 -0400

    Initial Commit

---
+++ PlacesIveBeen/PlacesIveBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesIveBeenApp.swift
+//  PlacesIveBeen
+//
+//  Created by Noah McGuire on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PlacesIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
